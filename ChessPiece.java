6527ff5faa8cf69bd68265da96aedb643b84c617
amelaaganovic <48222640+amelaaganovic@users.noreply.github.com>
2019-03-20 17:46:17 -0400

    Add files via upload

--- ChessPiece.java
+++ ChessPiece.java
@@ -1,27 +1,39 @@
-package W19Project3GIVETOSTUDENTS;
+package ChessVersion2;
 
 public abstract class ChessPiece implements IChessPiece {
 
-	private Player owner;
+    private Player owner;
 
-	protected ChessPiece(Player player) {
-		this.owner = player;
-	}
+    protected ChessPiece(Player player) {
+        this.owner = player;
+    }
 
-	public abstract String type();
+    public abstract String type();
 
-	public Player player() {
-		return owner;
-	}
+    public Player player() {
+        return owner;
+    }
 
-	public boolean isValidMove(Move move, IChessPiece[][] board) {
-		boolean valid = false;
 
-		//  THIS IS A START... More coding needed
-		
-		if (((move.fromRow == move.toRow) && (move.fromColumn == move.toColumn)) == false)
-			return valid;
 
-		return false;
-	}
+    public boolean isValidMove(Move move, IChessPiece[][] board) {
+        // Check turn
+        if (move.toRow >= 8 || move.toColumn >= 8)
+            return false;
+        if (move.toRow == move.fromRow && move.toColumn == move.fromColumn)
+            return false;
+        if (board[move.toRow][move.toColumn] != null &&
+                board[move.toRow][move.toColumn].player().equals(player()))
+            return false;
+
+        return true;
+    }
+
+
+
+
+    // return true if boards are different
+    public boolean compareBoard(Move move) {
+        return false;
+    }
 }
--- King.java
+++ King.java
@@ -1,5 +1,7 @@
-package W19Project3GIVETOSTUDENTS;
+package ChessVersion2;
 
+// Assume all comments were done by XUE unless otherwise stated
+import java.lang.Math;
 public class King extends ChessPiece {
 
 	public King(Player player) {
@@ -11,8 +13,37 @@ public class King extends ChessPiece {
 	}
 	
 	public boolean isValidMove(Move move, IChessPiece[][] board) {
-		boolean valid = true;
-        // More code is needed
-		return valid;
+		boolean valid = false;
+
+		System.out.print("...moving KING" +Math.abs(move.fromRow - move.toRow));
+		if(!super.isValidMove(move,board))
+			return false;
+
+			// MOVING FORWARD //
+			if (player() == Player.BLACK)
+				if (Math.abs(move.fromRow - move.toRow) == 0 || Math.abs(move.fromRow - move.toRow) == 1) {
+					if (Math.abs(move.toColumn - move.fromColumn) == 1 || (move.toColumn - move.fromColumn) == 0) {
+						if(board[move.toRow][move.toColumn] == null ||
+								board[move.toRow][move.toColumn].player() == Player.WHITE ) {
+							valid = true;
+							System.out.println("...moving 1 space...successful");
+						}
+					}
+				}
+			if (player() == Player.WHITE)
+				if (Math.abs(move.fromRow - move.toRow) == 0 || Math.abs(move.fromRow - move.toRow) == 1) {
+					if (Math.abs(move.toColumn - move.fromColumn) == 1 || (move.toColumn - move.fromColumn) == 0) {
+						if(board[move.toRow][move.toColumn] == null||
+								board[move.toRow][move.toColumn].player() == Player.BLACK) {
+							valid = true;
+							System.out.println("...moving 1 space...successful");
+						}
+					}
+				}
+		if(move.fromRow == move.toRow && move.fromColumn == move.toColumn)
+			valid = false;
+		if(!valid)
+			System.out.println("... invalid move");
+		return valid ;
 	}
 }
